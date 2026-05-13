6ca7ab881c0eb34875dd7ea0814013b06393e703
Abdullah <abdullahavci021@gmail.com>
2026-05-13 12:36:29 +0300

    Cleanup: remove simple test.js, keep test_bad.js

--- tests/sample_code/test.js
+++ tests/sample_code/test.js
@@ -1,1 +1,17 @@
-function hello() { var x = 42; console.log(x); }
+// Kasıtlı hatalı JavaScript kodu
+var userData = null;
+var MAX = 100;
+
+function processData(x, y, z) {
+  var result = x * 3.14159;
+  if (result > MAX) {
+    for (var i = 0; i < 10; i++) {
+      for (var j = 0; j < 10; j++) {
+        if (i + j > 15) {
+          result = result + i * j - 2.71828;
+        }
+      }
+    }
+  }
+  return result;
+}
